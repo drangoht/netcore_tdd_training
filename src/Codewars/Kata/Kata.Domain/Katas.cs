e9c9a837e4cfd7bcb34065dfad358aded4ae14b0
Grégory Thognard <drangoht@gmail.com>
2022-06-25 16:59:25 +0200

    init

---
+++ src/Codewars/Kata/Kata.Domain/Katas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Kata.Domain
+{
+    public class Katas
+    {
+        public static string ToCamelCase(string str)
+        {
+            var result = "";
+            if (str.Contains("_"))
+                result = ArrayToUpperCase(str.Split("_"));
+            if (str.Contains("-"))
+                result = ArrayToUpperCase(str.Split("-"));
+            return result;
+      }
+        private static string ArrayToUpperCase(string[] arrStr)
+        {
+            var camelCaseStr = arrStr[0];
+            for (int i=1;i<arrStr?.Length;i++)
+            {
+                camelCaseStr += arrStr[i].Substring(0,1).ToUpperInvariant() + arrStr[i].Substring(1);
+            }
+            return camelCaseStr;
+        }
+    }
+}
---
+++ src/Codewars/Kata/Kata.UnitTests/CamelCaseShould.cs
@@ -0,0 +1,27 @@
+using Kata.Domain;
+namespace Kata.UnitTests
+{
+    /// <summary>
+    /// Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+    /// The first word within the output should be capitalized only if the original word was capitalized 
+    /// (known as Upper Camel Case, also often referred to as Pascal case).
+    /// Examples
+    /// "the-stealth-warrior" gets converted to "theStealthWarrior"
+    /// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+    /// </summary>
+    public class CamelCaseShould
+    {
+        [Theory]
+        [InlineData("theStealthWarrior", "the_stealth_warrior")]
+        [InlineData("TheStealthWarrior", "The-Stealth-Warrior")]
+        [InlineData("zouBiDa", "zou_bi-da")]
+        public void ReturnCamelCaseWhenDashOrUnderscoreFound(string expected, string accept)
+        {
+            // Arrange
+            // Act
+            // Assert
+
+            Assert.Equal(expected, Katas.ToCamelCase(accept));
+        }
+    }
+}
